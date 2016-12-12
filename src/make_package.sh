732544cf9da79ccede757c25bca4a20833feb6ed
biaobean <biaobean@hotmail.com>
2016-12-12 18:17:35 +0800

    Updates src/make_package.sh
    Auto commit by GitBook Editor

---
+++ src/make_package.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+IMPALA_HOME=incubator-impala
+BUILD_DIR=impala
+#tar zcvf impala.tar.gz ${IMPALA_HOME} \
+#	--exclude="${IMPALA_HOME}/be/build/release/benchmarks" \
+#	--exclude="*.class" --exclude="*.o" --exclude="*.o" --exclude="${IMPALA_HOME}/thirdparty" --#exclude="${IMPALA_HOME}/.git" --exclude="${IMPALA_HOME}/tests" --exclude="${IMPALA_HOME}/Testing" --#exclude="${IMPALA_HOME}/testdata" --exclude="*.java" --exclude="*.cpp" --exclude="*.cc" --exclude="*.h" -#-exclude="expr-test"
+	
+mkdrir -p $BUILD_DIR
+cd $BUILD_DIR
+
+cp -r $IMPALA_HOME/bin .
+
+mkdir -p be/build/latest
+cp -r $IMPALA_HOME/be/build/latest/* be/build/latest
+rm -rf be/build/latest/benchmarks
+
+
+
+mkdir -p fe/target
+cp -r $IMPALA_HOME/fe/target/classes fe/target
+cp -r $IMPALA_HOME/fe/target/dependency fe/target
+cp -r $IMPALA_HOME/fe/target/test-classes fe/target
+#cp $IMPALA_HOME/fe/target/*.jar fe/target
+#rm fe/target/*-tests.jar
+cp $IMPALA_HOME/fe/target/impala-frontend-*-SNAPSHOT.jar fe/target
+
+cp -r $IMPALA_HOME/shell .
+
+
+tar zcvf impala.tar.gz ${IMPALA_HOME} \
+	--exclude="*-test"
